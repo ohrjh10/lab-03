2e09dcf201d6bf6bbf5564e10220646335707536
ohrjh10 <122945822+ohrjh10@users.noreply.github.com>
2023-02-13 19:16:46 -0800

    Final commit
    Cannot use git push so I'm uploading the files manually

--- mail_server.py
+++ mail_server.py
@@ -24,11 +24,18 @@ def load_mail() -> List[Dict[str, str]]:
 
 def save_mail(mail: List[Dict[str, str]]) -> None:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+
+    Arguments: the paremeter mail shows the list of dictionaries representing the mail entries
+    Summary: the mail entries are saved in the system but nothing is printed that shows that it has been saved
+    Returns: nothing is returned
     """
     thisdir.joinpath('mail_db.json').write_text(json.dumps(mail, indent=4))
 
 def add_mail(mail_entry: Dict[str, str]) -> str:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Arguments: the parameter mail_entry shows the list of dictionaries representing the mail entries
+    Summary: the mail is added to the dictionary of mail lists
+    Returns: string that shows the new mail entry
     """
     mail = load_mail()
     mail.append(mail_entry)
@@ -38,6 +45,9 @@ def add_mail(mail_entry: Dict[str, str]) -> str:
 
 def delete_mail(mail_id: str) -> bool:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Arguments: (mail_id:str) shows the id of the mail entry that will be deleted
+    Summary: the chosen mail is deleted from the list of emails
+    Returns: bool prints true if the mail was successfully deleted, and false otherwise
     """
     mail = load_mail()
     for i, entry in enumerate(mail):
@@ -50,6 +60,9 @@ def delete_mail(mail_id: str) -> bool:
 
 def get_mail(mail_id: str) -> Optional[Dict[str, str]]:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Arguments: the string that shows the id of the mail to get
+    Summary: it is used to get a specific mail entry from the list
+    Returns: the dictionary of the mail entry if there is a mail that matches the argument
     """
     mail = load_mail()
     for entry in mail:
@@ -60,6 +73,9 @@ def get_mail(mail_id: str) -> Optional[Dict[str, str]]:
 
 def get_inbox(recipient: str) -> List[Dict[str, str]]:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Arguments: recipient: str shows the mail entries for a recipient
+    Summary: gets all the mail entries that the recipient received
+    Returns: The list of dictionary that shows the mail entries
     """
     mail = load_mail()
     inbox = []
@@ -71,6 +87,9 @@ def get_inbox(recipient: str) -> List[Dict[str, str]]:
 
 def get_sent(sender: str) -> List[Dict[str, str]]:
     """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Arguments: sender: str shows the string of the sender of the mail entries
+    Summary: it is used to find mail entries from a specific sender
+    Returns: the list of dictionary that shows the mail sent by the corresponding sender
     """
     mail = load_mail()
     sent = []
@@ -106,8 +125,11 @@ def delete_mail_route(mail_id: str):
     Returns:
         bool: True if the mail was deleted, False otherwise
     """
+    res =jsonify({'deleted':delete_mail(mail_id)})
+    res.status_code = 200
+    return res
+
     # TODO: implement this function
-    pass # remove this line
 
 @app.route('/mail/<mail_id>', methods=['GET'])
 def get_mail_route(mail_id: str):
@@ -139,6 +161,11 @@ def get_inbox_route(recipient: str):
     res.status_code = 200
     return res
 
+@app.route('/mail/sent/<sender>', methods=['GET'])
+def get_sender_entry(sender: str):
+    res = jsonify(get_send(sender))
+    res.status_code = 200
+    return res
 # TODO: implement a rout e to get all mail entries for a sender
 # HINT: start with soemthing like this:
 #   @app.route('/mail/sent/<sender>', ...)
--- weather_client.py
+++ weather_client.py
@@ -7,17 +7,22 @@ from typing import Dict
 URL = "https://api.openweathermap.org/data/2.5/weather"
 
 # TODO: get an API key from openweathermap.org and fill it in here!
-API_KEY = ""
+API_KEY = "7239a449998f8efc32db810bd0f590c2"
 
 def get_weather(city) -> Dict:
     res = requests.get(URL, params={"q": city, "appid": API_KEY})
     return res.json()
 
 # TODO: try connecting to a another API! e.g. reddit (https://www.reddit.com/dev/api/)
+def get_cat_fact() -> Dict:
+    response = requests.get("https://catfact.ninja/fact")
+    return response.json()
 
 def main():
     temp = get_weather("London")
     print(temp)
+    print("\n")
+    print(get_cat_fact())
 
 if __name__ == "__main__":
     main()
